9c1cb91fcd4b8a7a1920498b7d1c30a989974373
robotcator <problemset@163.com>
2022-10-12 00:02:18 +0800

    cleangmem_tile.h

--- csrc/flash_attn/src/fmha/gmem_tile.h
+++ csrc/flash_attn/src/fmha/gmem_tile.h
@@ -86,27 +86,10 @@ struct Gmem_tile_qkv {
         // int64_t row_offset = (int64_t)row * params.qkv_stride_in_bytes;
         uint32_t row_offset = (uint32_t)(((use_seqlen_q ? binfo.sum_s_q : binfo.sum_s_k) + row) * row_stride_in_bytes);
         // Add the block index.
-#ifdef DEBUG_PRINT
-        if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-            printf("use_seqlen_q=%d\n", use_seqlen_q);
-            printf("threadIdx.x=%d, blockIdx.x=%d, blockIdx.y=%d, tidx=%d, row=%d, col=%d, THREADS_PER_ROW=%d\n",
-                threadIdx.x, blockIdx.x, blockIdx.y, tidx, row, col, THREADS_PER_ROW);
-            printf("threadIdx.x=%d, blockIdx.x=%d, blockIdx.y=%d, row_offset=%d ROWs=%d, COLS=%d, BYTES_PER_ROW=%d, THREADS_PER_ROW=%d, ROWS_PER_LDG=%d, LDGS=%d\n", 
-                threadIdx.x, blockIdx.x, blockIdx.y, row_offset, ROWS, COLS, BYTES_PER_ROW, THREADS_PER_ROW, ROWS_PER_LDG, LDGS);
-            printf("\n");
-        }
-#endif        
+      
         // row_offset += (int64_t)((binfo.sum_s * NUM_MATS + qkv_offset) * binfo.h + binfo.bidh) * BYTES_PER_ROW;
         row_offset += (uint32_t)(binfo.bidh * head_stride_in_elts * BYTES_PER_ELEMENT);
 
-// #ifdef DEBUG_PRINT
-//         if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-//             printf("use_seqlen_q=%d\n", use_seqlen_q);
-//             printf("threadIdx.x=%d, threadIdx.x=%d, blockIdx.y=%d, row_offset=%d BYTES_PER_LDG=%d\n", 
-//             threadIdx.x, threadIdx.x, blockIdx.y, row_offset, BYTES_PER_LDG);
-//             printf("\n");
-//         }
-// #endif
         // Assemble the final pointer.
         ptr += row_offset + col * BYTES_PER_LDG;
     }
@@ -233,16 +216,7 @@ struct Gmem_tile_o {
         row_offset += (uint32_t)(binfo.bidh * head_stride_in_elts * BYTES_PER_ELEMENT);
         // Assemble the final pointer.
         ptr_ += row_offset + col * BYTES_PER_STG;
-// #ifdef DEBUG_PRINT
-//         if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-//             printf("print o parameter\n");
-//             printf("threadIdx.x=%d, blockIdx.x=%d, blockIdx.y=%d, tidx=%d, row=%d, col=%d, THREADS_PER_ROW=%d\n",
-//                 threadIdx.x, blockIdx.x, blockIdx.y, tidx, row, col, THREADS_PER_ROW);
-//             printf("threadIdx.x=%d, blockIdx.x=%d, blockIdx.y=%d, row_offset=%d ROWs=%d, COLS=%d, BYTES_PER_ROW=%d, THREADS_PER_ROW=%d, ROWS_PER_STG=%d, LDGS=%d\n", 
-//                 threadIdx.x, blockIdx.x, blockIdx.y, row_offset, ROWS, COLS, BYTES_PER_ROW, THREADS_PER_ROW, ROWS_PER_STG, STGS);
-//             printf("\n");
-//         }
-// #endif
+
         // Is that thread active on the last STG?
         if( HAS_INCOMPLETE_STG ) {
             is_active_for_last_stg_ = row + (STGS - 1) * ROWS_PER_STG < Cta_tile::M;
@@ -520,16 +494,6 @@ struct Gmem_tile_mma_mask {
         uint32_t row_offset = bidx * params.mask_seq_mod_size * binfo.actual_seqlen_k * BYTES_PER_ELEMENT;
         row_offset += (uint32_t)( (row % params.mask_seq_mod_size) * binfo.actual_seqlen_k * BYTES_PER_ELEMENT); 
 
-#ifdef DEBUG_PRINT
-    if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-        printf("init mask tid_=%d, warp=%d, lane=%d, warp_n=%d, warp_m=%d, quad=%d, tid=%d, row=%d, col=%d\n",
-            tidx_, warp, lane, warp_n, warp_m, quad, tid, row, col);
-        printf("init mask bidb=%d, bidh=%d, param.h=%d, mask_head_mod_size=%d, mask_seq_mod_size=%d, loop_step_idx=%d\n", 
-            binfo.bidb, binfo.bidh, params.h, params.mask_head_mod_size, params.mask_seq_mod_size, loop_step_idx);
-        printf("\n");
-    }
-#endif
-        // do we need to move col first if seklen_k > cols
         ptr_ += row_offset;
     }
 
@@ -566,17 +530,6 @@ struct Gmem_tile_mma_mask {
                         //                 && ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= min(COLS, actual_seqlen_k));
                         preds[offset] = (current_row < min(ROWS, actual_seqlen_q))
                                         && ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= actual_seqlen_k);
-#ifdef DEBUG_PRINT
-                        if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0))  {
-                            printf("load mask mi=%d, ni=%d, ii=%d, jj=%d, offset=%d, current_row=%d, current_col=%d, start_ptr=%p, ptrs[offset]=%p, preds[offset]=%d\n",
-                                mi, ni, ii, jj, offset, current_row, current_col, ptr_, ptrs[offset], preds[offset]);
-                            printf("load mask ROWS=%d, actual_seqlen_q=%d, COLS=%d, actual_seqlen_k=%d, loop_step_idx=%d, cond1=%d, cond2=%d\n",
-                                ROWS, actual_seqlen_q, COLS, actual_seqlen_k, loop_step_idx,
-                                (current_row <= min(ROWS, actual_seqlen_q)),
-                                ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= min(COLS, actual_seqlen_k)));
-                            printf("\n");
-                        }
-#endif
                     }
                 }
 
@@ -686,14 +639,6 @@ struct Gmem_tile_mma_bias {
         // row_offset = (uint32_t)(row * row_stride_in_bytes);
         row_offset += (uint32_t)(row * binfo.actual_seqlen_k * BYTES_PER_ELEMENT);   
 
-#ifdef DEBUG_PRINT
-    if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-        printf("tid_=%d, warp=%d, lane=%d, warp_n=%d, warp_m=%d, quad=%d, tid=%d, row=%d, col=%d\n",
-            tidx_, warp, lane, warp_n, warp_m, quad, tid, row, col);
-        printf("bidb=%d, bidh=%d, param.h=%d, bias_mod_size=%d\n", binfo.bidb, binfo.bidh, params.h, params.bias_mod_size);
-        printf("\n");
-    }
-#endif
         // do we need to move col first if seklen_k > cols
         ptr_ += row_offset;
     }
@@ -725,17 +670,6 @@ struct Gmem_tile_mma_bias {
 
                         preds[offset] = (current_row < min(ROWS, actual_seqlen_q))
                                         && ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= actual_seqlen_k);
-#ifdef DEBUG_PRINT
-                        if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0))  {
-                            printf("load bias mi=%d, ni=%d, ii=%d, jj=%d, offset=%d, current_row=%d, current_col=%d, start_ptr=%p, ptrs[offset]=%p, preds[offset]=%d\n",
-                                mi, ni, ii, jj, offset, current_row, current_col, ptr_, ptrs[offset], preds[offset]);
-                            printf("load bias ROWS=%d, actual_seqlen_q=%d, COLS=%d, actual_seqlen_k=%d, loop_step_idx=%d, cond1=%d, cond2=%d\n",
-                                ROWS, actual_seqlen_q, COLS, actual_seqlen_k, loop_step_idx,
-                                (current_row <= min(ROWS, actual_seqlen_q)),
-                                ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= min(COLS, actual_seqlen_k)));
-                            printf("\n");
-                        }
-#endif
                     }
                 }
 
@@ -838,14 +772,7 @@ struct Gmem_tile_mma_ds {
         // the index of bs and head dim
         uint32_t row_offset = bidx * binfo.actual_seqlen_q * binfo.actual_seqlen_k * BYTES_PER_ELEMENT;
         // row_offset = (uint32_t)(row * row_stride_in_bytes);
-#ifdef DEBUG_PRINT
-    if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-        printf("ds tid_=%d, warp=%d, lane=%d, warp_n=%d, warp_m=%d, quad=%d, tid=%d, row=%d, col=%d\n",
-            tidx_, warp, lane, warp_n, warp_m, quad, tid, row, col);
-        printf("ds bidb=%d, bidh=%d, param.h=%d, blockIdx.x=%d\n", binfo.bidb, binfo.bidh, params.h, blockIdx.x);
-        printf("\n");
-    }
-#endif
+
         row_offset += (uint32_t)(row * binfo.actual_seqlen_k * BYTES_PER_ELEMENT);
         // do we need to move col first if seklen_k > cols
         ptr_ += row_offset;
@@ -879,12 +806,6 @@ struct Gmem_tile_mma_ds {
                         preds = (current_row < min(ROWS, actual_seqlen_q))
                                         && ((current_col + BYTES_PER_LDG / BYTES_PER_ELEMENT) <= actual_seqlen_k);
 
-#ifdef DEBUG_PRINT
-    if ((threadIdx.x == 0) && (blockIdx.x == 0) && (blockIdx.y == 0)) {
-        printf("ds store blockIdx.x=%d, mi=%d, ni=%d, ii=%d, jj=%d, current_row=%d, current_col=%d, float1=%f, float2=%f, begin=%p, ptrs=%p, preds=%d\n",
-            blockIdx.x, mi, ni, ii, jj, current_row, current_col, tmp00, tmp01, ptr_, ptrs, preds);
-    }
-#endif
                         if (preds) {
                             fmha::stg(ptrs, dst);
                         }
