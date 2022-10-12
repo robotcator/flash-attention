15ade00d6b14f7abf33b90ddf69cb70976a19a55
robotcator <problemset@163.com>
2022-10-12 11:07:28 +0800

    remove useless benchmark code

--- benchmarks/correctness/test_time.sh
+++
@@ -1,5 +0,0 @@
-python benchmarks/correctness/check_speed_forward.py --has_mask_bias=false 2>&1 |tee no_mask_bias_test.txt
-python benchmarks/correctness/check_speed_forward.py --has_mask_bias=true 2>&1 |tee has_mask_bias_test.txt
-
-python benchmarks/correctness/check_speed_backward.py --has_mask_bias=false 2>&1 |tee no_mask_bias_train.txt
-python benchmarks/correctness/check_speed_backward.py --has_mask_bias=true 2>&1 |tee has_mask_bias_train.txt
